f72e658bfa0df7c1725a636c2902159540a40ab2
Keneth <45386437+kberriom@users.noreply.github.com>
2024-05-30 10:24:49 -0500

    Disable Android SplashScreen fade out animation

--- android/app/src/main/kotlin/com/universitysystem/front/university_system_front/MainActivity.kt
+++ android/app/src/main/kotlin/com/universitysystem/front/university_system_front/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.universitysystem.front.university_system_front
 
+import android.os.Build
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            // Disable splash screen fade out animation to avoid flicker
+            splashScreen.setOnExitAnimationListener {splashScreenView -> splashScreenView.remove()}
+        }
+        super.onCreate(savedInstanceState)
+    }
+}
