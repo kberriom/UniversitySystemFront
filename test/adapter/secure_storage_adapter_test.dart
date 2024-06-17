7713d605e4b1637c80159c4c0f9e37f945f97f1e
Keneth <45386437+kberriom@users.noreply.github.com>
2024-06-17 18:05:58 -0500

    Create secure_storage_adapter_test.dart

---
+++ test/adapter/secure_storage_adapter_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:test/test.dart';
+import 'package:university_system_front/Adapter/secure_storage_adapter.dart';
+
+void main() {
+  group('Test mock secure storage', () {
+    const key = "jwt";
+    const expectedResult = "expectedResult";
+
+    setUp(() {
+      FlutterSecureStorage.setMockInitialValues({"": "", key: expectedResult});
+    });
+
+    test('read from storage', () async {
+      final notSavedValue = await SecureStorageAdapter().readValue("random");
+      final savedValue = await SecureStorageAdapter().readValue(key);
+
+      expect(notSavedValue, null);
+      expect(savedValue, expectedResult);
+    });
+
+    test('write in storage', () async {
+      const otherResult = "other expectedResult";
+      await SecureStorageAdapter().writeValue(key, otherResult);
+
+      final savedValue = await SecureStorageAdapter().readValue(key);
+
+      expect(savedValue, otherResult);
+    });
+
+    test('delete from storage', () async {
+      await SecureStorageAdapter().deleteValue(key);
+
+      final savedValue = await SecureStorageAdapter().readValue(key);
+
+      expect(savedValue, null);
+    });
+  });
+}
