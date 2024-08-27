8017587a595bca40a2d683fee735d764bd213309
Keneth <45386437+kberriom@users.noreply.github.com>
2024-08-27 14:25:24 -0500

    Add animation to HomeTextTitle
    Add slide and fade animations

--- lib/Widget/home/common_home_widgets.dart
+++ lib/Widget/home/common_home_widgets.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/animation_mixin/animation_mixin.dart';
 
-class HomeTextTitle extends StatelessWidget {
-
+class HomeTextTitle extends StatefulWidget {
   final String text;
 
   const HomeTextTitle({
-    super.key, required this.text,
+    super.key,
+    required this.text,
   });
+
+  @override
+  State<HomeTextTitle> createState() => _HomeTextTitleState();
+}
+
+class _HomeTextTitleState extends State<HomeTextTitle> with AnimationMixin {
+  final slideTween = Tween(begin: const Offset(-0.4, 0), end: const Offset(0, 0));
+  final fadeTween = Tween(begin: 0.0, end: 1.0);
+  late final AnimationController fadeInController;
 
   @override
   Widget build(BuildContext context) {
@@ -17,19 +27,43 @@ class HomeTextTitle extends StatelessWidget {
         child: FractionallySizedBox(
           widthFactor: 0.7,
           child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10, left: 16),
-              child: Text(
-                overflow: TextOverflow.ellipsis,
-                maxLines: 1,
-                text,
-                style: const TextStyle(fontFamily: 'blazma', fontStyle: FontStyle.italic, fontSize: 32),
+            SlideTransition(
+              position: controller.drive(slideTween),
+              child: FadeTransition(
+                opacity: fadeInController.drive(fadeTween),
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 10, left: 16),
+                  child: Text(
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 1,
+                    widget.text,
+                    style: const TextStyle(fontFamily: 'blazma', fontStyle: FontStyle.italic, fontSize: 32),
+                  ),
+                ),
               ),
             ),
-            Divider(color: Theme.of(context).colorScheme.onSurface),
+            SlideTransition(
+              position: controller.drive(slideTween),
+              child: Divider(color: Theme.of(context).colorScheme.onSurface),
+            ),
           ]),
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    fadeInController = createController();
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!controller.isForwardOrCompleted) {
+      fadeInController.animateTo(1.0, curve: Curves.easeIn, duration: Durations.extralong1);
+      controller.animateTo(1.0, curve: Curves.easeOut, duration: Durations.long1);
+    }
   }
 }
