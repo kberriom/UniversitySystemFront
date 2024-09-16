8b273714716eea559bde05d733fffc16c0c794c7
Keneth <45386437+kberriom@users.noreply.github.com>
2024-09-16 15:55:13 -0500

    Add leading buttons on Windows

--- lib/Widget/navigation/leading_widgets.dart
+++ lib/Widget/navigation/leading_widgets.dart
@@ -13,42 +13,62 @@ class UniSystemAppBarLeading extends _$UniSystemAppBarLeading {
   @override
   Widget? build() {
     if (PlatformUtil.isWindows) {
-      return IconButton(
-        onPressed: () {
-          ref.read(currentThemeModeProvider.notifier).changeThemeMode();
-        },
-        icon: Icon(ref.watch(currentThemeModeProvider) == ThemeMode.light ? Icons.sunny : Icons.nightlight_round_sharp),
-      );
+      return const UniSystemThemeButton();
     }
     return null;
   }
 
   void setLeading(Widget? leading) {
-    if (!PlatformUtil.isAndroid) {
+    if (PlatformUtil.isWindows) {
       state = leading;
     }
   }
 }
 
-class VisibilityLeadingWrapper extends ConsumerWidget {
-  const VisibilityLeadingWrapper({super.key, required this.pageKey, required this.leading, required this.widget});
+Widget setLeadingOnWindows(ValueKey<String> key, Widget widget, {UniSystemSmartLeadButton leading = const UniSystemBackButton()}) {
+  if (PlatformUtil.isWindows) {
+    switch (leading) {
+      case UniSystemBackButton():
+        return VisibilityLeadingWrapper<UniSystemBackButton>(
+          pageKey: key,
+          leading: leading,
+          widget: widget,
+        );
+      case UniSystemCloseButton():
+        return VisibilityLeadingWrapper<UniSystemCloseButton>(
+          pageKey: key,
+          leading: leading,
+          widget: widget,
+        );
+    }
+  } else {
+    return widget;
+  }
+}
 
+class VisibilityLeadingWrapper<LT extends UniSystemSmartLeadButton> extends ConsumerStatefulWidget {
   final ValueKey pageKey;
   final Widget widget;
-  final Widget leading;
+  final LT leading;
+
+  const VisibilityLeadingWrapper({super.key, required this.pageKey, required this.leading, required this.widget});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<VisibilityLeadingWrapper> createState() => _VisibilityLeadingWrapperState<LT>();
+}
+
+class _VisibilityLeadingWrapperState<LT extends UniSystemSmartLeadButton> extends ConsumerState<VisibilityLeadingWrapper> {
+  @override
+  Widget build(BuildContext context) {
     return VisibilityDetector(
-      key: pageKey,
+      key: widget.pageKey,
       onVisibilityChanged: (VisibilityInfo info) {
-        if (info.visibleFraction > 0 && ref.read(uniSystemAppBarLeadingProvider) is! UniSystemBackButton) {
-          _setPostFrameLeadingChange(ref, leading);
+        if (info.visibleFraction > 0 && ref.read(uniSystemAppBarLeadingProvider) is! LT) {
+          _setPostFrameLeadingChange(ref, widget.leading);
         }
       },
       child: Builder(builder: (context) {
-        _setPostFrameLeadingChange(ref, leading);
-        return widget;
+        return widget.widget;
       }),
     );
   }
@@ -58,15 +78,71 @@ class VisibilityLeadingWrapper extends ConsumerWidget {
       ref.read(uniSystemAppBarLeadingProvider.notifier).setLeading(leading);
     });
   }
+
+  @override
+  void initState() {
+    _setPostFrameLeadingChange(ref, widget.leading);
+    super.initState();
+  }
 }
 
-class UniSystemBackButton extends ConsumerWidget {
+class UniSystemThemeButton extends ConsumerStatefulWidget {
+  const UniSystemThemeButton({super.key});
+
+  @override
+  ConsumerState<UniSystemThemeButton> createState() => _UniSystemThemeButtonState();
+}
+
+class _UniSystemThemeButtonState extends ConsumerState<UniSystemThemeButton> {
+  late Brightness _brightness;
+
+  @override
+  void didChangeDependencies() {
+    _brightness = Theme.of(context).brightness;
+    super.didChangeDependencies();
+  }
+
+@override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        setState(() {
+          _brightness = ref.read(currentThemeModeProvider.notifier).changeThemeMode(Theme.of(context).brightness);
+        });
+      },
+      icon: Icon(_brightness == Brightness.light ? Icons.sunny : Icons.nightlight_round_sharp),
+    );
+  }
+}
+
+sealed class UniSystemSmartLeadButton implements Widget {}
+
+class UniSystemBackButton extends ConsumerWidget implements UniSystemSmartLeadButton {
   const UniSystemBackButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return BackButton(onPressed: () {
-      ref.read(goRouterInstanceProvider).routerDelegate.pop();
+      ref.read(goRouterInstanceProvider).routerDelegate.popRoute();
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) => VisibilityDetectorController.instance.notifyNow());
+    });
+  }
+}
+
+class UniSystemCloseButton extends ConsumerWidget implements UniSystemSmartLeadButton {
+  final String? route;
+  final Object? extra;
+  const UniSystemCloseButton({super.key, this.route, this.extra});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return CloseButton(onPressed: () {
+      if (route == null) {
+        ref.read(goRouterInstanceProvider).routerDelegate.pop();
+      }else {
+        ref.read(goRouterInstanceProvider).pushReplacement(route!, extra: extra);
+      }
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) => VisibilityDetectorController.instance.notifyNow());
     });
   }
 }
